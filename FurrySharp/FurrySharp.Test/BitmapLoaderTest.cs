132e00a4d312d2ac54878957fa6d50e770c06b7f
Ludwig <ludwigsquirrel@gmail.com>
2024-10-08 13:48:37 -0700

    Added a homebrew bitmap loader to the project.

---
+++ FurrySharp/FurrySharp.Test/BitmapLoaderTest.cs
@@ -0,0 +1,38 @@
+﻿using FurrySharp.Utilities;
+
+namespace FurrySharp.Test;
+
+public class BitmapLoaderTest
+{
+    [Test]
+    public void LoadTest()
+    {
+        var data = BitmapLoader.LoadPixelData("test.bmp"); // 3x3 bitmap with 4 bits per pixel
+        // Iterate over the data. Print data in a grid.
+        for (int y = 0; y < data.GetLength(1); y++)
+        {
+            for (int x = 0; x < data.GetLength(0); x++)
+            {
+                Console.Write(data[x, y]);
+            }
+
+            Console.WriteLine();
+        }
+
+        /*
+         * Expected output:
+         * 012
+         * 120
+         * 201
+         */
+        Assert.That(data[0, 0], Is.EqualTo(0));
+        Assert.That(data[1, 0], Is.EqualTo(1));
+        Assert.That(data[2, 0], Is.EqualTo(2));
+        Assert.That(data[0, 1], Is.EqualTo(1));
+        Assert.That(data[1, 1], Is.EqualTo(2));
+        Assert.That(data[2, 1], Is.EqualTo(0));
+        Assert.That(data[0, 2], Is.EqualTo(2));
+        Assert.That(data[1, 2], Is.EqualTo(0));
+        Assert.That(data[2, 2], Is.EqualTo(1));
+    }
+}
---
+++ FurrySharp/FurrySharp/Utilities/BitmapLoader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace FurrySharp.Utilities;
+
+public static class BitmapLoader
+{
+    public static int[,] LoadPixelData(string filePath)
+    {
+        {
+            using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using var reader = new BinaryReader(stream);
+
+            // Read the bitmap file header
+            reader.BaseStream.Seek(0x12, SeekOrigin.Begin);
+            int widthInPixels = reader.ReadInt32();
+            int heightInPixels = reader.ReadInt32();
+
+            // Read the pixel data offset
+            reader.BaseStream.Seek(0xA, SeekOrigin.Begin);
+            int pixelDataOffset = reader.ReadInt32();
+
+            // Read the bits per pixel
+            reader.BaseStream.Seek(0x1C, SeekOrigin.Begin);
+            int bitsPerPixel = reader.ReadInt16();
+            int bytesPerScanline = (int)Math.Ceiling((bitsPerPixel * widthInPixels) / 32.0) * 4;
+
+            // Read the pixel data
+            reader.BaseStream.Seek(pixelDataOffset, SeekOrigin.Begin);
+            int[,] indices = new int[widthInPixels,heightInPixels];
+            switch (bitsPerPixel)
+            {
+                case 4:
+                    IterateAt4BitsPerPixel();
+                    break;
+                case 8:
+                    IterateAt8BitsPerPixel();
+                    break;
+                default:
+                    throw new NotSupportedException("Only 4 and 8 bits per pixel are supported.");
+            }
+
+            return indices;
+
+            void IterateAt4BitsPerPixel()
+            {
+                for (int j = heightInPixels - 1; j >= 0; j--)
+                {
+                    byte[] bytes = reader.ReadBytes(bytesPerScanline);
+                    for (int i = 0; i < widthInPixels; i++)
+                    {
+                        var getFrontHalf = i % 2 == 0;
+                        var byteIndex = i / 2;
+                        var byteValue = bytes[byteIndex];
+                        var shift = getFrontHalf ? 4 : 0;
+                        var mask = 0b1111 << shift;
+                        var index = (byteValue & mask) >> shift;
+                        indices[i,j] = index;
+                    }
+                }
+            }
+
+            void IterateAt8BitsPerPixel()
+            {
+                for (int j = 0; j < heightInPixels; j++)
+                {
+                    byte[] bytes = reader.ReadBytes(bytesPerScanline);
+                    for (int i = 0; i < widthInPixels; i++)
+                    {
+                        indices[i,j] = bytes[i];
+                    }
+                }
+            }
+        }
+    }
+}
