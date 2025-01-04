18e665ce18e4d67ec215298a410d838ea6d9e825
Ludwig <ludwigsquirrel@gmail.com>
2025-01-04 11:59:25 -0800

    Added algorithm to check if a line segment intersects a circle.

---
+++ FurrySharp/FurrySharp.Test/CircleLineIntersectionTests.cs
@@ -0,0 +1,25 @@
+﻿using FurrySharp.Utilities;
+using Microsoft.Xna.Framework;
+
+namespace FurrySharp.Test;
+
+public static class CircleLineIntersectionTests
+{
+    [TestCase(-10, 0, 1, 0, 0, 1, 0, false, 0)]
+    [TestCase(0, 0, 1, 0, 0, 1, 0, true, 1, 1f, 0f)]
+    [TestCase(0, 0, 1, -1, 0, 0, 1, true, 2, -1, 0, 0, 1)]
+    public static void TestCircleLineIntersection(float cx, float cy, float r, float x1, float y1, float x2, float y2, bool intersects, int intersections, params float[] points)
+    {
+        var circleCenter = new Vector2(cx, cy);
+        var radius = r;
+        var lineStart = new Vector2(x1, y1);
+        var lineEnd = new Vector2(x2, y2);
+        var result = CircleLineIntersection.Intersect(circleCenter, radius, lineStart, lineEnd);
+        Assert.That(result.Intersects, Is.EqualTo(intersects));
+        Assert.That(result.Intersections, Is.EqualTo(intersections));
+        for (int i = 0; i < points.Length; i += 2)
+        {
+            Assert.That(result.IntersectionPoints[i / 2], Is.EqualTo(new Vector2(points[i], points[i + 1])));
+        }
+    }
+}
---
+++ FurrySharp/FurrySharp/Utilities/CircleLineIntersection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace FurrySharp.Utilities;
+
+public struct CircleLineIntersectionResult
+{
+    public List<Vector2> IntersectionPoints;
+    public int Intersections => IntersectionPoints.Count;
+    public bool Intersects => Intersections > 0;
+}
+
+public static class CircleLineIntersection
+{
+    public static CircleLineIntersectionResult Intersect(Vector2 circleCenter, float radius, Vector2 lineStart, Vector2 lineEnd)
+    {
+        CircleLineIntersectionResult result = new()
+        {
+            IntersectionPoints = new List<Vector2>(2)
+        };
+        // Translate everything to the center for simpler calculations.
+        lineStart.X -= circleCenter.X;
+        lineStart.Y -= circleCenter.Y;
+        lineEnd.X -= circleCenter.X;
+        lineEnd.Y -= circleCenter.Y;
+
+        float dx = lineEnd.X - lineStart.X;
+        float dy = lineEnd.Y - lineStart.Y;
+
+        // Quadratic equation coefficients.
+        float a = dx * dx + dy * dy;
+        float b = 2 * ((lineStart.X * dx) + (lineStart.Y * dy));
+        float c = lineStart.X * lineStart.X + lineStart.Y * lineStart.Y - radius * radius;
+        
+        // A negative discriminant tells us that there are no real values for t.
+        float discriminant = b * b - 4 * a * c;
+        if (discriminant < 0)
+        {
+            return result;
+        }
+        float sqrtdiscriminant = MathF.Sqrt(discriminant);
+        float t1 = (-b - sqrtdiscriminant) / (2 * a);
+        float t2 = (-b + sqrtdiscriminant) / (2 * a);
+
+        // Check if the intersection points are on the line segment (between 0 and 1).
+        if (t1 is >= 0 and <= 1)
+        {
+            result.IntersectionPoints.Add(new Vector2(lineStart.X + t1 * dx, lineStart.Y + t1 * dy));
+        }
+        
+        if (t2 is >= 0 and <= 1)
+        {
+            result.IntersectionPoints.Add(new Vector2(lineStart.X + t2 * dx, lineStart.Y + t2 * dy));
+        }
+
+        return result;
+    }
+}
